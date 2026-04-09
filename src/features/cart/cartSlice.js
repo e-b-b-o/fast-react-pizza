d3d8e19c490ad2a86382bd02a3980cc00965ce03
Ebisa Getachew <ebisagetachew1243@gmail.com>
2026-04-09 23:20:18 +0300

    added use fetcher for fecthing menu

--- src/features/cart/cartSlice.js
+++ src/features/cart/cartSlice.js
@@ -2,15 +2,7 @@ import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
   //
-  cart: [
-    {
-      pizzaId: 12,
-      name: "Mediterraniun",
-      quantity: 2,
-      unitPrice: 16,
-      totalPrice: 32,
-    },
-  ],
+  cart: [],
 };
 
 const cartSlice = createSlice({
